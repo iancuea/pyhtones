c2cc3d79fbf16cca25ed01fe826045f60177147b
1lucaglaz <luca.galaz@alumnos.uach.cl>
2026-06-22 17:20:37 -0300

    tab KPIs

--- dashboard/preprocesamiento.py
+++ dashboard/preprocesamiento.py
@@ -30,43 +30,17 @@ if __name__ == "__main__":
     print("Iniciando pipeline de optimizacion completo...\n")
     
     optimizar_base_de_datos(
-        ruta_origen="dashboard/csv_dashboard/BaseINDICES-2020-2025.csv",
-        ruta_destino="dashboard/csv_dashboard/clean_indices_dashboard.csv",
-        columnas_a_dejar=[
-            "Año", 
-            "Nombre Region", 
-            "Carrera Genérica", 
-            "Matrícula primer año hombres", 
-            "Matrícula primer año mujeres", 
-            "Matrícula Total", 
-            "Valor de arancel"
-        ],
-        separador=";",
-        encoding="utf-8"
-    )
-    
-    optimizar_base_de_datos(
-        ruta_origen="dashboard/csv_dashboard/todas_las_ingenierias_chile.csv",
-        ruta_destino="dashboard/csv_dashboard/clean_kpis_nacional.csv",
-        columnas_a_dejar=[
-            "Institución", 
-            "Carrera", 
-            "Retención de 1er año", 
-            "Empleabilidad al 2º Año", 
-            "Duración Real (semestres)", 
-            "Ingreso promedio al 4° año"
-        ],
-        separador=";",
-        encoding="utf-16"
-    )
-    
-    optimizar_base_de_datos(
-        ruta_origen="csv/ArchivoB_Adm2025.csv",
-        ruta_destino="dashboard/csv_dashboard/clean_demre_2025.csv",
-        columnas_a_dejar=[
-            "ID_aux", 
-            "GRUPO_DEPENDENCIA"
-        ],
-        separador=";",
-        encoding="utf-8"
-    )
+    ruta_origen="dashboard/csv_dashboard/todas_las_ingenierias_chile.csv",
+    ruta_destino="dashboard/csv_dashboard/clean_kpis.csv",
+    columnas_a_dejar=[
+        "Institución", 
+        "Carrera", 
+        "Retención de 1er año", 
+        "Empleabilidad al 1er año",
+        "Empleabilidad al 2º Año", 
+        "Duración Real (semestres)", 
+        "Ingreso promedio al 4° año"
+    ],
+    separador=";",
+    encoding="utf-16"
+)
